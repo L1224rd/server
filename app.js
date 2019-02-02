0652b5098a543915704c05383cc3f330b9e4d4a7
Eder Marques <eder.marques@citi.org.br>
2019-02-02 20:50:27 -0300

    Initial Commit as a server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
 // ==================== INTERNAL IMPORTS ==================== //
 
@@ -10,13 +11,30 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    console.log(file);
+    // error first callback
+    cb(null, './uploads/');
+  },
+  filename: (req, file, cb) => {
+    // error first callback
+    cb(null, file.originalname)
+  }
+});
+
+const upload = multer({ storage })
+
 // ==================== MIDDLEWARE ==================== //
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
 });
 
 // parse application/x-www-form-urlencoded
@@ -28,23 +46,39 @@ app.use(bodyParser.json());
 // serving static files
 app.use('/views', express.static(path.join(__dirname, 'views')));
 
+
 // ==================== FUNCTIONS ==================== //
 
 // returns the full path of the passed view
-const getViewPath = view => path.join(__dirname, `views/${view}/${view}.html`);
+const getViewPath = view => path.join(__dirname, `views/${view}/${view}.ejs`);
 
 // ==================== ROUTES ==================== //
 
 // ==================== RENDER VIEWS ==================== //
 
 app.get('/', (req, res) => {
-    res.sendFile(getViewPath('home'));
+  res.render(getViewPath('home'), {
+    name: 'Server',
+    like: ['a', 'b', 'c']
+  });
+});
+
+app.get('/upload', (req, res) => {
+  res.render(getViewPath('upload'));
+});
+
+app.post('/upload', upload.single('myfile'), (req, res) => {
+  res.redirect('/upload');
+});
+
+app.get('/download', (req, res) => {
+  res.sendFile(req.query.file);
 });
 
 // ==================== START SERVER ==================== //
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log('READY');
+  console.log('READY');
 });
 
 // ====================================================== //
